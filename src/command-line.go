bd2bd383dfdf2a595e7358a6fbbcfb8438e9d72a
arthur.pacheco@tatic.net <arthur.pacheco@tatic.net>
2021-01-26 13:52:12 -0300

    Adiciona métodos específicos para acesso à REST API

--- src/command-line.go
+++ src/command-line.go
@@ -5,40 +5,67 @@ import (
 	"os"
 )
 
+func launchJob(job *string) {
+	//TODO
+}
+
 func launch(job *string) {
 	if *job == "" {
 		print("Job id must not be null!")
+	} else {
+		launchJob(job)
 	}
-	print("Launching job...")
+}
+
+func deleteAllRunningJobs() {
+	//TODO
+}
+
+func deleteAllJobsOnQueue() {
+	//TODO
+
+}
+
+func deleteJob(job *string) {
 	//TODO
 }
 
 func delete(job *string) {
 	if *job == "jobs" {
 
-		//TODO delete running jobs
+		deleteAllRunningJobs()
 
 	} else if *job == "queue" {
 
-		//TODO queue jobs
+		deleteAllJobsOnQueue()
 
 	} else if *job == "" {
 
-		print("Job id must not be null!")
+		deleteJob(job)
 
 	}
-	print("Deleting job...")
+}
+
+func statusRunningJobs() {
+	//TODO
+}
+
+func statusQueueJobs() {
+	//TODO
+}
+
+func jobStatus(job *string) {
 	//TODO
 }
 
 func status(job *string) {
 	if *job == "jobs" {
 
-		//TODO status jobs
+		statusRunningJobs()
 
 	} else if *job == "queue" {
 
-		//TODO queue jobs
+		statusQueueJobs()
 
 	} else if *job == "" {
 
@@ -46,24 +73,36 @@ func status(job *string) {
 
 	}
 
-	print("Job status...")
-	//TODO job status
+	jobStatus(job)
+
 }
 
 func main() {
+
 	command := flag.String("command", "", "Command{launch|delete|status} (Required)")
+
 	job := flag.String("job_id", "", "Job ID")
+
 	flag.Parse()
 
 	switch *command {
+
 	case "launch":
+
 		launch(job)
+
 	case "delete":
+
 		delete(job)
+
 	case "status":
+
 		status(job)
+
 	default:
+
 		print("Invalid command. Acepted values: {launch|delete|status}")
 		os.Exit(1)
+
 	}
 }
