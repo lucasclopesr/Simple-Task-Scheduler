60c4f54b64dbf333580b1313e25cd3f8de007de1
Pedro <ptcar@inspr.com>
2021-01-30 21:04:01 -0300

    feat: manejamento de memória no job handler

--- cmd/simpd/jobhandler/jh.go
+++ cmd/simpd/jobhandler/jh.go
@@ -2,8 +2,11 @@ package jobhandler
 
 import (
 	"github.com/lucasclopesr/Simple-Task-Scheduler/cmd/simpd/api/handlers"
+	"github.com/lucasclopesr/Simple-Task-Scheduler/cmd/simpd/memory"
+	"github.com/lucasclopesr/Simple-Task-Scheduler/cmd/simpd/processes"
 	"github.com/lucasclopesr/Simple-Task-Scheduler/cmd/simpd/queue"
 	"github.com/lucasclopesr/Simple-Task-Scheduler/pkg/meta"
+	"github.com/lucasclopesr/Simple-Task-Scheduler/pkg/simperr"
 )
 
 // NewJobHandler creates a job handler to allocate resources for jobs
@@ -11,35 +14,39 @@ func NewJobHandler() handlers.JobHandler {
 	return &jobHandler{}
 }
 
-type jobHandler map[string]meta.Job
+type jobHandler struct{}
 
 // CreateJob validates job and inserts into queue
 func (j jobHandler) CreateJob(s string, jr meta.JobRequest) error {
-	//print(jr.User)
-	//print(jr.Job.ID)
+	if _, err := memory.GetJob(s); err == nil {
+		return simperr.NewError().Build()
+	}
 	queue := queue.GetQueueManager()
 	err := queue.InsertJobIntoQueue(jr.Job)
-
+	if queue.Len() == 1 {
+		processes.GetProcessManager().CreateFirstJob()
+	}
+	memory.CreateJob(s, jr.Job)
 	return err
 }
 
 // DeleteJob deletes a job from queue
 func (j jobHandler) DeleteJob(s string) error {
+	if _, err := memory.GetJob(s); err != nil {
+		return simperr.NewError().Build()
+	}
 	queue := queue.GetQueueManager()
 	_, err := queue.DeleteJobFromQueue(s)
-
+	memory.DeleteJob(s)
 	return err
 }
 
 // GetJob finds job of ID s and returns in the format meta.Job
-func (j jobHandler) GetJob(s string) (meta.Job, error) {
-	queue := queue.GetQueueManager()
-	job, err := queue.GetJobFromQueue(s)
+func (j jobHandler) GetJob(s string) (job meta.Job, err error) {
 
-	if err == nil {
-		return job, nil
+	if job, err = memory.GetJob(s); err != nil {
+		return job, simperr.NewError().Build()
 	}
-
 	return job, err
 }
 
