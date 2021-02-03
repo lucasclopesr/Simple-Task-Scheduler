ea0a826ad25ff6f6e310ad7dbb74c5f373c377b7
Pedro <ptcar@inspr.com>
2021-02-03 15:07:18 -0300

    fux: muda o link de download

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,9 @@
 #!bash
 
-curl -LO https://github.com/lucasclopesr/Simple-Task-Scheduler/releases/download/v0.0.1/simp
+curl -LO https://github.com/lucasclopesr/Simple-Task-Scheduler/releases/download/v0.1.0-rc/simp
 sudo install simp /usr/local/bin
 
-curl -LO https://github.com/lucasclopesr/Simple-Task-Scheduler/releases/download/v0.0.1/simpd
+curl -LO https://github.com/lucasclopesr/Simple-Task-Scheduler/releases/download/v0.1.0-rc/simpd
 sudo install simpd /usr/local/bin
 
 rm simp simpd
