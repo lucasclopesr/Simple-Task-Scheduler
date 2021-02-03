6610804b60922e25ede1893218badb75ee198ba5
Pedro <ptcar@inspr.com>
2021-02-03 00:27:08 -0300

    feat: script de instalação

---
+++ scripts/install.sh
@@ -0,0 +1,9 @@
+#!bash
+
+curl -LO https://github.com/lucasclopesr/Simple-Task-Scheduler/releases/download/v0.0.1/simp
+sudo install simp /usr/local/bin
+
+curl -LO https://github.com/lucasclopesr/Simple-Task-Scheduler/releases/download/v0.0.1/simpd
+sudo install simpd /usr/local/bin
+
+rm simp simpd
